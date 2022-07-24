1809cd826768fe548201692d1908cdb8c1ae0347
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-07-24 10:49:05 +0200

    keep dialog open

--- src/virus-dialog.js
+++ src/virus-dialog.js
@@ -2,7 +2,7 @@ import pepe from "./pepe-gif.gif";
 import "./virus-dialog.css";
 
 export const VirusDialog = () => (
-  <dialog className="virus-dialog">
+  <dialog className="virus-dialog" open>
     <img className="virus-dialog__pepe" src={pepe} alt="laughing pepe" />
     <span>Oh No! You've been hacked!!!</span>
   </dialog>
